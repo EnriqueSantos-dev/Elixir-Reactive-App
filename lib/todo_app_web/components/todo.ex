af873855936b143f9d525400022af080f6e28107
Enrique Santos <santosenrique869@gmail.com>
2022-12-05 15:36:33 -0300

    feat: create todo component

---
+++ lib/todo_app_web/components/todo.ex
@@ -0,0 +1,23 @@
+defmodule TodoAppWeb.Todo do
+  use Phoenix.Component
+
+  def todo(assigns) do
+    ~H"""
+      <div class="flex space-x-3 min-h-12 border border-zinc-300 rounded items-center p-2">
+        <button 
+          class="w-5 h-5 rounded-full border border-zinc-300"
+          phx-click="toggle_todo_status"
+          phx-value-id={@todo.id}
+          style={if @todo.completed, do: "background-color: rgb(34,197,94);", else: "background-color: transparent"}
+          />
+        <p 
+          class="flex-1"
+          style={if @todo.completed, do: "text-decoration: line-through; opacity: 0.7;"}
+        >
+          <%= @todo.description %>
+        </p>
+        <button phx-click="remove_todo" phx-value-id={@todo.id}>❌</button>
+      </div>
+    """
+  end
+end
---
+++ lib/todo_app_web/live/todo_live.ex
@@ -0,0 +1,100 @@
+defmodule TodoAppWeb.TodoLive do
+  use TodoAppWeb, :live_view
+
+  def mount(assign, _params, socket) do
+    todos = []
+    filtered_todos = todos
+    active_filter = "all"
+
+    socket = 
+      socket
+      |> assign(:todos, todos)
+      |> assign(:filtered_todos, filtered_todos)
+      |> assign(:active_filter, active_filter)
+      |> actives_todos()
+
+    {:ok, socket}
+  end
+
+  def handle_event("new_todo_form", %{"new_todo" => %{"message" => message}}, socket) do
+    todo = %{id: Ecto.UUID.generate(), description: message, completed: false}
+
+    socket =
+      socket
+      |> assign(:todos, [todo] ++ socket.assigns.todos)
+      |> actives_todos()
+      |> filter_todos()
+
+    {:noreply, socket}
+  end
+
+  def handle_event("select_all_todos", _params, socket) do
+    socket = 
+      socket
+      |> assign(:todos, 
+          Enum.map(socket.assigns.todos, fn todo -> %{todo | completed: true} end)
+          )
+      |> actives_todos()
+      |> filter_todos()
+
+    {:noreply, socket}
+  end
+
+  def handle_event("remove_todo", %{"id" => id}, socket) do
+    socket =
+      socket
+      |> assign(:todos, Enum.filter(socket.assigns.todos, fn todo -> todo.id != id end))
+      |> filter_todos()
+    
+      {:noreply, socket}
+  end
+
+  def handle_event("change_filter", %{"filter" => filter}, socket) do
+    socket =
+      socket
+      |> assign(:active_filter, filter)
+      |> actives_todos()
+      |> filter_todos()
+
+    {:noreply, socket}
+  end
+
+  def handle_event("toggle_todo_status", %{"id" => id}, socket) do
+    socket =
+      socket 
+      |> assign(:todos, Enum.map(socket.assigns.todos, fn todo -> 
+        if todo.id == id do
+          %{todo | completed: not todo.completed}
+        else
+          todo
+        end
+      end))
+      |> actives_todos()
+      |> filter_todos()
+
+    {:noreply, socket}
+  end
+
+  defp filter_todos(socket) do
+    socket
+    |> assign(:filtered_todos,
+      Enum.filter(socket.assigns.todos, fn todo ->
+        case socket.assigns.active_filter do
+          "all" ->
+            true
+
+          "active" ->
+            not todo.completed
+
+          "completed" ->
+            todo.completed
+        end
+      end)
+    )
+  end
+
+  def actives_todos(socket) do
+    socket
+      |> assign(:active_todos, Enum.count(socket.assigns.todos, fn t -> t.completed end))
+  end
+end
