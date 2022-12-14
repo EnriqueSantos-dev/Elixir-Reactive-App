5ff2d5b5d5e384795d69f7032599d777122c0420
Carlos Daniel <65622154+Carloss-Daniel@users.noreply.github.com>
2022-12-14 16:32:13 -0300

    style: format code to correct style guide elixir

--- lib/todo_app_web/components/button.ex
+++ lib/todo_app_web/components/button.ex
@@ -3,9 +3,9 @@ defmodule TodoAppWeb.Button do
 
   def button(assigns) do
     ~H"""
-      <button 
-        class="rounded border p-2 font-semibold" 
-        phx-click="change_filter" 
+      <button
+        class="rounded border p-2 font-semibold"
+        phx-click="change_filter"
         phx-value-filter={@filter}
         style={if @active_filter == @filter, do: "background-color: rgb(37, 99, 235); color: white;", else: "background-color: transparent; color: rgb(63, 66,70);"}
         >
--- lib/todo_app_web/components/todo.ex
+++ lib/todo_app_web/components/todo.ex
@@ -4,13 +4,13 @@ defmodule TodoAppWeb.Todo do
   def todo(assigns) do
     ~H"""
       <div class="flex space-x-3 min-h-12 border border-zinc-300 rounded items-center p-2">
-        <button 
+        <button
           class="w-5 h-5 rounded-full border border-zinc-300"
           phx-click="toggle_todo_status"
           phx-value-id={@todo.id}
           style={if @todo.completed, do: "background-color: rgb(34,197,94);", else: "background-color: transparent"}
           />
-        <p 
+        <p
           class="flex-1"
           style={if @todo.completed, do: "text-decoration: line-through; opacity: 0.7;"}
         >
